48cf06d87c6485fc73996350a3d4f503191550d9
gomjellie <gomjellie@naver.com>
2017-01-07 05:27:14 +0900

    Access-Control-Allow-Origin설정을 안해도 되네! 번역기능은 잘됨

--- js/script.js
+++ js/script.js
@@ -1,8 +1,10 @@
 
 //팝업 페이지의 #src 입력된 값이 변경 되었을때
-if (document.querySelector("#src")) {
-    document.querySelector('#src').addEventListener('input', function() {
-        var src = document.querySelector('#src').value;
-        document.querySelector('#result').innerText = src;//"command list:\n[help, option, who made this?, reset, donate]";
+if (document.querySelector("#usr_input")) {
+    document.querySelector('#usr_input').addEventListener('input', function() {
+        var src = document.querySelector('#usr_input').value;
+
+        translate(src);
+        //document.querySelector('#result').innerText = //"command list:\n[help, option, who made this?, reset, donate]";
     });
 }
